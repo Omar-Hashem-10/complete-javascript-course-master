a201ceb10be029ca3703533f2ae91aeadf488962
omar hashem <omarhashem20051310@gmail.com>
2026-07-17 22:40:22 +0300

    Values, Variables  and naming convention with assignment

---
+++ 01-Fundamentals-Part-1/assignments/Values-and-Variables/script.js
@@ -0,0 +1,13 @@
+/*
+1. Declare variables called country, continent and population and assign their values according to your own country (population in millions).
+
+2. Log their values to the console.
+*/
+
+let country = "Egypt";
+let continent = "Africa";
+let population = 120;
+
+console.log(country);
+console.log(continent);
+console.log(population);
--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,5 +1,37 @@
-let js = "amazing";
+// let js = "amazing";
 
-if (js === "amazing") alert("JavaScript is FUN!");
+// if (js === "amazing") alert("JavaScript is FUN!");
 
 console.log(40 + 8 + 23 - 10);
+
+// camel case convention
+let firstName = "Omar";
+
+// under snake case convention 
+let middle_name = "Hashem";
+
+// pascal case convention
+let LastName = "Fathallah";
+
+/* Unexpected naming convention 
+    ##### no reserved keywords 
+    let new or function but we can make it legal - How? 
+    add $ or _ before word 
+    let $new or _function this legal 
+    #####
+    numbers before variable name but you make it middle or end variable  and special character middle in variable name and space between words or hyphens 
+*/
+
+
+console.log(firstName + " " + middle_name + " " + LastName); 
+
+// when the variable don't change anymore you must declaration type const not let and the naming convention the variable name should all character capital  
+const PI = 3.1415; 
+
+
+// you should make variable name more descriptive not just variable name
+let myFirstJob = "Programmer";
+let myCurrentJob = "Teacher";
+
+let job1 = "Programmer";
+let job2 = "Teacher";
